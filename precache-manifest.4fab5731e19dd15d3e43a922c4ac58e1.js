62015585203e83d122cf46369a24a20af1aa7476
Ayush yadav <yayush2002@gmail.com>
2021-09-09 15:14:09 +0530

    first commit

---
+++ precache-manifest.4fab5731e19dd15d3e43a922c4ac58e1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d8c7bce9e47d0946e4a51c7b3e2e80",
+    "url": "/assets/img/header.png"
+  },
+  {
+    "revision": "48025182349af4abfe3e272a7e128c4c",
+    "url": "/assets/img/introduction-music.png"
+  },
+  {
+    "revision": "90840bfca190b45f7cd04ed9d8c2cd54",
+    "url": "/assets/img/introduction-users.png"
+  },
+  {
+    "revision": "eba639c1b0a9287dd3a1904b9ae9831f",
+    "url": "/assets/img/profile-pic.jpg"
+  },
+  {
+    "revision": "d899ba7d2cac3f11c4377a700d68d22e",
+    "url": "/assets/img/song-header.png"
+  },
+  {
+    "revision": "e7b9aeb02c86a922404d13073d21836a",
+    "url": "/assets/img/user-header.png"
+  },
+  {
+    "revision": "d87f1d1e360c7b9a81a9",
+    "url": "/css/app.2b90af19.css"
+  },
+  {
+    "revision": "6d046040e44992a2b6aa",
+    "url": "/css/chunk-vendors.06a7122d.css"
+  },
+  {
+    "revision": "8273cfb12326ea87cabecc9a96f400f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d87f1d1e360c7b9a81a9",
+    "url": "/js/app.eb73b642.js"
+  },
+  {
+    "revision": "e54c5e62a0f9b2a5d4de",
+    "url": "/js/chunk-2d21a3d2.32836d72.js"
+  },
+  {
+    "revision": "dea3faabca78dca0b097",
+    "url": "/js/chunk-2d22d746.c6f23b9f.js"
+  },
+  {
+    "revision": "6d046040e44992a2b6aa",
+    "url": "/js/chunk-vendors.501296d1.js"
+  },
+  {
+    "revision": "b5e7cc3971dec24f137c",
+    "url": "/js/groupedChunk.63217345.js"
+  },
+  {
+    "revision": "ec96b182f67283292ae8a8539a47be21",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
